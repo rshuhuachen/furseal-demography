ac93e14e84ea4faa01dc8b16a5be08c2302846f5
David Vendrami <david.vendrami@student.unife.it>
2022-03-10 15:27:55 +0100

    Update SF_1.sh

--- C) Figures/SF_1.sh
+++ C) Figures/SF_1.sh
@@ -6,7 +6,7 @@ tail -1 AFS_RAD_alt4_allids_maxL_$i/AFS_RAD_alt4_allids_maxL_MSFS.obs >> All_SFS
 done
 
 # Also get only the SFS entry from the empirical SFS:
-tail -1  > Emp.sfs
+tail -1 AFS_RAD_alt4_allids_MAFpop0.obs > Emp.sfs
 
 # Open R to get mean and sd values for each SFS class based on the 600 simulated files.
 ### R ###
@@ -16,3 +16,7 @@ sds<-apply(data,2,function(x) sd(x))
 write.table(cbind(avs,sds),"Simulated_avs_and_sds.txt",quote=F,row.names=F,col.names=F,sep="\t")
 
 # These can then be easily plotted against the empirical SFS
+emp<-read.table("Emp.sfs",h=f)
+data<-read.table("Sim_aver.txt",h=F)
+barplot(data[-c(1,2),1])
+barplot(emp[-c(1,2),1])
