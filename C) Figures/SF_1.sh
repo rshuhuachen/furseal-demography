5008af4e2ec9ca559e3c7abc57855631f6b3d146
David Vendrami <david.vendrami@student.unife.it>
2022-03-10 15:26:10 +0100

    Update SF_1.sh

--- C) Figures/SF_1.sh
+++ C) Figures/SF_1.sh
@@ -1,9 +1,12 @@
-# First let's get all of the simluated SFS (see ) in a single file
+# First let's get all of the simluated SFS (see "Simulate_SFS.sh") in a single file.
 
 for i in {1..600}
 do
 tail -1 AFS_RAD_alt4_allids_maxL_$i/AFS_RAD_alt4_allids_maxL_MSFS.obs >> All_SFS.txt
 done
+
+# Also get only the SFS entry from the empirical SFS:
+tail -1  > Emp.sfs
 
 # Open R to get mean and sd values for each SFS class based on the 600 simulated files.
 ### R ###
@@ -12,3 +15,4 @@ avs<-apply(data,2,function(x) mean(x))
 sds<-apply(data,2,function(x) sd(x))
 write.table(cbind(avs,sds),"Simulated_avs_and_sds.txt",quote=F,row.names=F,col.names=F,sep="\t")
 
+# These can then be easily plotted against the empirical SFS
